190bdbf6f65f203c7b67faf7906b5303f1f0641d
samtweedie <76115945+samtweedie@users.noreply.github.com>
2023-01-31 16:26:06 +0000

    dev branch

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,10 +28,17 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(13F, 32F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(1894, 1009);
+            this.Name = "Form1";
+            this.Text = "Memory Game";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
